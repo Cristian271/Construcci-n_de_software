f15d7391e52b2a7261fdf622677932699e346027
AnelisseRivera <anelisseriveramanzanero@MacBook-Air-de-Anelisse-2.local>
2026-09-10 16:07:48 -0600

    feat: Implementacion clase GestorReportes

--- ReportesUniversitarios/src/main/java/GestorReportes.java
+++ ReportesUniversitarios/src/main/java/GestorReportes.java
@@ -1,5 +1,35 @@
 import java.util.ArrayList;
 
 public class GestorReportes {
+    private ArrayList<Reporte> listaReportes;
+    private int contador = 0;
+    public GestorReportes(){
+        listaReportes = new ArrayList<Reporte>();
+    }
+
+    public void agregarReporte(Reporte nuevoReporte){
+        listaReportes.add(nuevoReporte);
+        contador++;
+    }
+
+    public Reporte buscarReporte(String idBuscado){
+        for (Reporte reporte: listaReportes) {
+            if (reporte.getIdentificador().equals(idBuscado)) {
+                return reporte;
+            }
+        }
+        return null;
+    }
+
+    public void actualizarEstado(Reporte reporteAcualizar, Estado nuevoEstado){
+        reporteAcualizar.setEstado(nuevoEstado);
+    }
+
+    public void imprimirReportes(){
+        for(Reporte reporte: listaReportes){
+            reporte.obtenerDetalle();
+        }
+    }
+
 
 }
